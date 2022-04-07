a318f7c593a3043ddfb867dd30992fcef244d05a
Nikhil Batra <nikhilb1906@gmail.com>
2022-04-07 22:20:29 +0530

    hello

--- src/main/java/com/citibank/carlist/service/carServiceImpl.java
+++ src/main/java/com/citibank/carlist/service/carServiceImpl.java
@@ -1,5 +1,6 @@
 package com.citibank.carlist.service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,8 +9,21 @@ import com.citibank.carlist.entities.Car;
 import com.citibank.carlist.repository.carRepository;
 
 public class carServiceImpl implements carService{
+	//return "Car {" + "carID=" + carID + ", carName=" + carName + ", engineCC=" + engineCC + ", transmission="
+	//		+ transmission + ", seatingcapacity=" + seatingcapacity + ", ncapRating=" + ncapRating + ", color="
+	//		+ color + "}";
+	
+	
 	
 	List<Car> list;
+	
+	public carServiceImpl(){
+		list = new ArrayList<>();
+		list.add(new Car( 1, "Maruti", 1000, "Automatic", 5, 4, "White"));
+		list.add(new Car( 2, "Honda", 1000, "Automatic", 5, 4, "Grey"));
+		list.add(new Car( 3, "Tata", 1000, "Manual", 5, 4, "Brown"));
+		list.add(new Car( 4, "Mahindra", 1000, "Manual", 5, 4, "Silver"));
+	}
 	
 	@Autowired
 	private carRepository carrepository;
